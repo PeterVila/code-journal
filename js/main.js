dc0c4e35910a33c8b6cc3d34a2cb526ef9982b71
Peter Vila <peterdvila@gmail.com>
2021-09-07 15:00:15 -0700

    Listened for submit events on the journal entry form and put the form inptu values into a new object, added nextEntryId to the object, incremented, preprending the new object to the entries in the data model, Reset the image previews src attribute, and reset the form inputs

--- js/main.js
+++ js/main.js
@@ -1,11 +1,43 @@
 /* global data */
 /* exported data */
-var $imgUrl = document.querySelector('input[id="photoUrl');
+var $imgUrl = document.querySelector('input[id="photoUrl"]');
+var $formSubmit = document.querySelector('form');
+var $input = document.querySelectorAll('input');
+var $textArea = document.querySelector('textarea')
+var $setImg = document.querySelector('img');
 
+//Listening for input changes for IMAGE
 $imgUrl.addEventListener('input', changeImg)
-
 function changeImg(event){
     console.log(event.target.value);
-    var $setImg = document.querySelector('img');
     $setImg.setAttribute('src', event.target.value)
 }
+
+//Listening for submit event
+console.log($formSubmit)
+$formSubmit.addEventListener('submit', logForm)
+function logForm(event){
+    event.preventDefault();
+    var formObject = {
+        'title': $input[0].value,
+        'photoUrl': $input[1].value,
+        'notes': $textArea.value,
+        'nextEntryId': data.nextEntryId,
+    }
+    data.nextEntryId += 1;
+    data.entries.unshift(formObject);
+    $setImg.setAttribute('src', 'images/placeholder-image-square.jpg');
+    $input[0].value = "",
+    $input[1].value = "",
+    $textArea.value = "",
+    console.log(data)
+}
+
+
+// Put the form 's input values into a new object.
+// Add the nextEntryId to the object.
+// Increment the nextEntryId on the data model.
+// Prepend the new object to the entries in the data model.
+// Reset the image preview 's `src'
+// attribute.
+// Reset the form inputs.
